5a33fe799c49aefbfbdff3c1e79bca18da58260f
Adewale Adetoro <adetorodev19@gmail.com>
2023-08-03 21:59:18 +0100

    excercise

--- 02-dart.dart
+++ 02-dart.dart
@@ -10,18 +10,38 @@
 // Create a program that asks the user for a
 //number and then prints out a list of
 //all the divisors of that number.
-import 'dart:io';
+// import 'dart:io';
+
+// void main() {
+//   stdout.write("Enter a number: ");
+//   String? n = stdin.readLineSync();
+
+//   if (n != null) {
+//     int number = int.parse(n);
+//     for (var i = 1; i <= number; i++) {
+//       if (number % i == 0) {
+//         print(i);
+//       }
+//     }
+//   }
+// }
+
+// Take two lists, for example:
+
+//   a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+
+//   b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+//  Save
+// and write a program that returns a list that
+// contains only the elements that are
+// common between them (without duplicates). Make sure
+// your program works on two lists of different sizes.
 
 void main() {
-  stdout.write("Enter a number: ");
-  String? n = stdin.readLineSync();
+  List<int> a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89];
+  List<int> b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
 
-  if (n != null) {
-    int number = int.parse(n);
-    for (var i = 1; i <= number; i++) {
-      if (number % i == 0) {
-        print(i);
-      }
-    }
+  for (var i = 0;){
+    if()
   }
 }
